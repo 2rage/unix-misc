93a53aeb4f346d79930590b132e4000ed0c069b8
2rage <the2rage@icloud.com>
2024-04-24 20:25:21 +0300

    Update hugo python script

--- py/hugo-server.py
+++ py/hugo-server.py
@@ -2,6 +2,9 @@ import subprocess
 import re
 
 def hugo_server():
+
+    cwd_path = '/home/the2rage/work/personal-site' # Путь к рабочей директории Hugo
+
     # Запускаем команду 'ip addr show eth0' и получаем вывод
     result = subprocess.run(['ip', 'addr', 'show', 'eth0'], stdout=subprocess.PIPE, text=True)
     
@@ -9,13 +12,14 @@ def hugo_server():
     ip_match = re.search(r'inet (\d+\.\d+\.\d+\.\d+)', result.stdout)
     if ip_match:
         wsl2_ipaddress = ip_match.group(1)
-        # Запускаем сервер Hugo с полученным IP-адресом
+        # Запускаем сервер Hugo в указанной директории
         subprocess.run([
             'hugo', 'server',
             '--bind', wsl2_ipaddress,
             '--baseURL', f'http://{wsl2_ipaddress}',
             '-D', '-F', '--gc', '-w'
-        ])
+        ], cwd=cwd_path)
+    return
 
 # Вызываем функцию для запуска Hugo сервера
 hugo_server()
