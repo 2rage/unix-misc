ea54bef510ef52b5bfcd6e842c3e0332ff3f132c
2rage <the2rage@icloud.com>
2024-06-04 16:31:25 +0300

    added new port input

--- py/check_port.py
+++ py/check_port.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import socket
 from check_ip import get_ip_address
 
@@ -15,7 +17,32 @@ def check_port(host, port):
     finally:
         sock.close()
 
+def get_port_from_user():
+    max_attempts = 3
+    attempts = 0
+
+    while attempts < max_attempts:
+        port = input("Введите номер порта: ")
+        try:
+            port = int(port)
+            if 1 <= port <= 65535:
+                return port
+            else:
+                print("Номер порта должен быть в диапазоне от 1 до 65535")
+        except ValueError:
+                print("Введите корректное число")
+
+        attempts += 1
+        if attempts < max_attempts:
+            print(f'Попытка {attempts} из {max_attempts}. Попробуйте снова.')
+        else:
+            print('Превышено количество попыток. Завершение программы')
+
 if __name__ == "__main__":
     host = get_ip_address()  # Измените на IP или хостнейм, который нужно проверить
-    port = 7777
-    check_port(host, port)
+     
+    port = get_port_from_user()
+    if port is not None:
+        check_port(host, port)
+    else:
+        print("Не удалось получить корректный номер порта.")
